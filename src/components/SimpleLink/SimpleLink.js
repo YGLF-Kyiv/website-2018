b6180faff6125efd41d8703a5e7f242389defad6
Marina Sol <marina.sol@railsreactor.com>
2017-10-27 16:30:56 +0300

    changed default colour for simple link and added more props

--- src/components/SimpleLink/SimpleLink.js
+++ src/components/SimpleLink/SimpleLink.js
@@ -2,17 +2,22 @@ import React, { PropTypes as toBe } from 'react';
 import './simple-link.scss';
 import Link from 'gatsby-link';
 
-export default class Header extends React.Component {
+export default class SimpleLink extends React.Component {
   static propTypes = {
     title: toBe.string,
     text: toBe.string,
     url: toBe.string,
+    target: toBe.string,
+  }
+
+  static defaultProps = {
+    target: '_blank',
   }
 
   render() {
-    const { title, text, url } = this.props;
+    const { title, text, url, target } = this.props;
     return (
-      <a className="simple-link" href={url} target="_blank" rel="noopener noreferrer" title={title || text}>
+      <a className="simple-link" href={url} target={target} rel="noopener noreferrer" title={title || text}>
         {text}
       </a>
     );
