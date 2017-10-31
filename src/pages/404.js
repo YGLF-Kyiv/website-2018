c47f1468a53abf097a210676ccd410b47bda6313
Andrei Sierkov <andrei.sierkov@railsreactor.com>
2017-10-31 23:27:33 +0200

    Fixed 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import Intro from '../components/Intro/Intro';
 
 const NotFoundPage = () => (
   <div className="not-found">
-    <Intro />
     <div className="container-fluid">
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
