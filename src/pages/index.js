d91e511828358c17aa9e9b09a2e933ffbcf5fd39
Marina Sol <marina.sol@railsreactor.com>
2017-11-24 21:04:17 +0100

    adding new lea text and moving speakers block

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,30 +3,16 @@ import './index.scss';
 import InfoBlock from '../components/InfoBlock/InfoBlock';
 import VenueBlock from '../components/VenueBlock/VenueBlock';
 import SpeakersIntroBlock from '../components/SpeakersIntroBlock/SpeakersIntroBlock';
-import SpeakerBlock from '../components/SpeakerBlock/SpeakerBlock';
 import speakersIntroData from '../assets/speakersIntro.json';
-import speakerData from '../assets/speakers.json';
 
 export default class IndexPage extends React.Component {
   render() {
     return (
       <div className="page-index lines-bg">
         <InfoBlock />
-
         <div className="container container-fluid">
-          <div className="highlighted-speaker">
-            <h3>Highlighted Speaker</h3>
-            <SpeakerBlock
-              speaker={speakerData.highlighted}
-              imageSize={{width: '220', height: '247'}}
-            />
-            <a href="/speakers" className="button-speakers">
-              See All Speakers
-            </a>
-          </div>
           <SpeakersIntroBlock data={speakersIntroData.callForPapers} />
         </div>
-
         <VenueBlock />
       </div>
     );
