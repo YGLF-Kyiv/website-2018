af681234eb744d7a8ccd0fec3683901ae0198b0a
Marina Sol <marina.sol@railsreactor.com>
2017-12-13 19:08:32 +0200

    FIxing long anchors - right spacing

--- src/components/SpeakerBlock/SpeakerBlock.js
+++ src/components/SpeakerBlock/SpeakerBlock.js
@@ -3,6 +3,7 @@ import './speaker-block.scss';
 import SocialIcons from '../SocialIcons/SocialIcons';
 import LazyLoad from 'react-lazyload';
 import { isChrome } from '../../utils/environment';
+import { getAnchorName } from '../../utils/common';
 
 export default class SpeakerBlock extends Component {
 
@@ -10,8 +11,14 @@ export default class SpeakerBlock extends Component {
     data: toBe.object,
   };
 
+  static defaultProps = {
+    data: {},
+  }
+
   render() {
-    const { data } = this.props;
+    const {
+      data: { first_name, last_name, image_src, social_icons, position, company, description }
+    } = this.props;
     const imageExtension = isChrome() ? 'webp' : 'jpg';
 
     return (
@@ -19,29 +26,29 @@ export default class SpeakerBlock extends Component {
         <div className="speaker-block-cols">
           <a
             href=""
-            name={`${data.first_name}-${data.last_name}`}
+            name={getAnchorName([first_name, last_name])}
             className="-no-outline anchor"
           />
           <div className="speaker-block-img">
             <LazyLoad offset={150}>
               <img
-                src={`/speakers/${data.image_src}.${imageExtension}`}
+                src={`/speakers/${image_src}.${imageExtension}`}
                 className="drop-shadow"
-                alt={`${data.first_name} ${data.last_name}`}
+                alt={`${first_name} ${last_name}`}
               />
             </LazyLoad>
-            <SocialIcons urls={data.social_icons} />
+            <SocialIcons urls={social_icons} />
           </div>
           <div className="speaker-block-text">
             <h3>
-              <span className="speaker-first-name">{data.first_name}</span>{' '}
-              <span className="speaker-last-name">{data.last_name}</span>
+              <span className="speaker-first-name">{first_name}</span>{' '}
+              <span className="speaker-last-name">{last_name}</span>
             </h3>
             <p>
-              { data.position && <span className="speaker-position">{data.position}</span> }
-              { data.company && <span className="speaker-company">{data.company}</span> }
+              { position && <span className="speaker-position">{position}</span> }
+              { company && <span className="speaker-company">{company}</span> }
             </p>
-            <p dangerouslySetInnerHTML={{__html: data.description}} />
+            <p dangerouslySetInnerHTML={{__html: description}} />
           </div>
         </div>
       </div>
--- src/utils/common.js
+++ src/utils/common.js
@@ -1,3 +1,12 @@
 export function isInBrowser() {
   return typeof window !== 'undefined';
 }
+
+export function replaceSpaces(str) {
+  return str.replace(/ /, '-');
+}
+
+export function getAnchorName(arr) {
+  // ['anna luise', 'jones'] => `anna-luise-jones`
+  return arr.map(el => replaceSpaces(el)).join('-');
+}
