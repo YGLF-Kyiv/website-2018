1419b8a716c991999008d20561848a937c5f6d02
Marina Sol <marina.sol@railsreactor.com>
2018-01-30 20:17:16 +0200

    putting back things from master

---
+++ data/speakersIntro.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "callForPapers": {
+    "number": "1",
+    "subtitle": "Amazing Talks",
+    "title": "Call For Papers",
+    "description": [
+      "This year YGLF is hosting amazing speakers from all around the world. Listed below are our keynote speakers. For the full lineup you can visit our speakers page. Make sure to check back regularly, as this is just the beginning, there is a lot more to come!"
+    ],
+    "actionButton": {
+      "href": "mailto:hello@yglf.com.ua?subject=Call%20For%20Papers",
+      "text": "Apply"
+    }
+  }
+}
