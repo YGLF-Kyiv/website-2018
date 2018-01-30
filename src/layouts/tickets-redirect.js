95412a5a3f49ff443f712ca54c4e90fcce89347e
Marina Sol <marina.sol@railsreactor.com>
2018-01-30 19:26:58 +0200

    Update meta descriptions

--- src/layouts/tickets-redirect.js
+++ src/layouts/tickets-redirect.js
@@ -12,6 +12,15 @@ export default class TicketsRedirectLayout extends React.Component {
         <Helmet>
           <title>{config.siteTitle}</title>
           <meta name="description" content={config.siteDescription} />
+          <meta property="og:title" content={config.siteDescription} />
+          <meta property="og:url" content={config.website} />
+          <meta property="og:description" content={config.siteDescription} />
+          <meta property="og:type" content="website" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content={`@${config.userTwitter}`} />
+          <meta name="twitter:creator" content={`@${config.userTwitter}`} />
+          <meta name="twitter:title" content={config.twitterTitle} />
+          <meta name="twitter:description" content={config.siteDescription} />
 
           <meta charset="utf-8" />
           <meta http-equiv="refresh" content={`1; url=${config.ticketsUrl}`} />
