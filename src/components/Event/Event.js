4bb407bdfcc9cde972f5d4328146a4e9d43252cb
Marina Sol <marina.sol@railsreactor.com>
2018-03-15 13:03:26 +0200

    some fixes

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -101,7 +101,9 @@ export default class Event extends React.Component {
               </FitToRhythm>
               { !!description.length && (
                 <div className={classNames('description', { '-can-show-more': showReadMore })}>
-                  {description.map(item => <p key={item} dangerouslySetInnerHTML={{ __html: item }} />) }
+                  { description.map(item =>
+                    <p key={item} dangerouslySetInnerHTML={{ __html: item }} />
+                  ) }
                 </div>
               ) }
             </div>
