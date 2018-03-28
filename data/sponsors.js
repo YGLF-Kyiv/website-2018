c3d72577843f6ba42a7143dab26dd6c3f0af47cd
Marina Sol <marina.sol@railsreactor.com>
2018-03-28 21:51:18 +0300

    fix styles for sponsors

--- data/sponsors.js
+++ data/sponsors.js
@@ -36,8 +36,8 @@ module.exports = {
       "url": "https://myheritage.com",
       "imageSrc": "/sponsors/my_heritage",
       "description": [
-        "Some text here. Among company’s clients leading global brands such as Adidas, L’Oréal, Under Armour, Jimmy Choo, Lacoste, and Versace.",
-        "The passionate team of 600+ dedicated, diverse industry and technology experts has decades of e-commerce experience and more than 400 site implementations under its belt to address the complex challenges, advancing technologies and unique needs of global markets.",
+        "Among company’s clients leading global brands such as Adidas, L’Oréal, Under Armour, Jimmy Choo, Lacoste, and Versace.",
+        "The passionate team of 600+ dedicated, diverse industry and technology experts has decades of e-commerce experience and more than 400 site implementations under its belt to address the complex challenges.",
       ],
     },
     {
