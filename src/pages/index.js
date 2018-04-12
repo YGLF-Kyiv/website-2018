d1c4d71d94572cdb2a4e743461e1bf40f8e2da66
Marina Sol <marina.sol@railsreactor.com>
2018-04-12 23:21:05 +0300

    Hide venue address, make all sponsor names links

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import './index.scss';
 import SpeakersIntroBlock from '../components/SpeakersIntroBlock/SpeakersIntroBlock';
 import Workshops from '../components/Workshops/Workshops';
 import HightlightedSpeakerBlock from '../components/HightlightedSpeakerBlock/HighlightedSpeakerBlock';
-import OrderedBlock from '../components/OrderedBlock/OrderedBlock';
+// import OrderedBlock from '../components/OrderedBlock/OrderedBlock';
 
 import OrderedBlockData from '../../data/orderedBlock.js';
 
@@ -17,7 +17,7 @@ export default class IndexPage extends React.Component {
         <div className="container container-fluid">
           <Workshops />
           <SpeakersIntroBlock data={OrderedBlockData.lightningTalks} />
-          <OrderedBlock data={OrderedBlockData.venue} showSubheader={false} />
+          { /* <OrderedBlock data={OrderedBlockData.venue} showSubheader={false} /> */ }
         </div>
       </div>
     );
