f255f8b783d0322dc813f83d83371bc7aaed4ff2
Yuri Tkachenko <yuri@ya.ru>
2018-04-15 15:55:40 +0300

    add twitter pixel

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -75,6 +75,20 @@ export default class Header extends React.Component {
         <script src="/libs/createjs.min.js" />
 
         {/*<link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet" />*/}
+
+        <script>
+          {`
+  !function (e, t, n, s, u, a) {
+  e.twq || (s = e.twq = function () {
+    s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments);
+  }, s.version = '1.1', s.queue = [], u = t.createElement(n), u.async = !0, u.src = '//static.ads-twitter.com/uwt.js',
+    a = t.getElementsByTagName(n)[0], a.parentNode.insertBefore(u, a))
+  }(window, document, 'script');
+  // Insert Twitter Pixel ID and Standard Event data below
+  twq('init','nzc0l');
+  twq('track','PageView');
+          `}
+        </script>
       </Helmet>
     );
   }
