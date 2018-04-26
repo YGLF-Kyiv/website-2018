8ae3e1208ffb289242ae191d08bb5229f4f19fa7
Yuri Tkachenko <yuri@ya.ru>
2018-04-26 13:47:51 +0300

    better title

--- src/pages/dou.js
+++ src/pages/dou.js
@@ -6,7 +6,7 @@ export default class DouPage extends React.Component {
     return (
       <div className="page-promo lines-bg">
         <div className="container-fluid">
-          <h2 className="promo-title">Specially for DOU People</h2>
+          <h2 className="promo-title">Specially For DOU Friends</h2>
           <div className="promo-tickets" id="2event_tickets_widget"></div>
           <div className="code"><code className="code-text">YGLFLovesDOU</code></div>
         </div>
